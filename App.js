148f67bc21f5b1edd2160c1f209a77e37340ba5a
willetcreek <doferrell@bbandt.com>
2015-10-22 18:39:39 -0400

    our first grid working!

---
+++ App.js
@@ -0,0 +1,37 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+	
+	launch: function() {
+	console.log('Our first app woot!');
+	this._loadData();
+	},
+	
+	// Get data from Rally
+	_loadData: function() {
+var myStore = Ext.create('Rally.data.wsapi.Store', {
+model: 'User Story',
+autoLoad: true,
+listeners: {
+load: function(myStore, myData, success) {
+console.log('got data!', myStore, myData,success);
+this._loadGrid(myStore);
+},
+scope: this
+},				
+fetch: ['FormattedID', 'Name', 'ScheduleState']
+});
+},
+	
+// Create and Show Grid of given stories
+_loadGrid: function(myStoryStore) {     
+var myGrid = Ext.create('Rally.ui.grid.Grid', {
+store: myStoryStore,
+columnCfgs: [
+'FormattedID', 'Name', 'ScheduleState']
+});
+
+this.add(myGrid);
+console.log('what is this?', this);
+}
+});
